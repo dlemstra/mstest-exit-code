7714ef7a3ff21c1c5912634ec000a410b7f9f26a
Dirk Lemstra <dirk@git.imagemagick.org>
2017-09-21 07:31:58 +0200

    Try to use other vstest.console as suggested in https://github.com/Microsoft/vstest/issues/1113

--- run-test.cmd
+++ run-test.cmd
@@ -4,8 +4,10 @@ call "VsDevCmd.cmd"
 
 msbuild mstest-exit-code.sln /p:Configuration=Debug
 
-vstest.console UnitTestProject\bin\Debug\UnitTestProject.dll
+set vstest="%VSINSTALLDIR%\Common7\IDE\Extensions\TestPlatform\vstest.console.exe"
 
-where vstest.console
+%vstest% UnitTestProject\bin\Debug\UnitTestProject.dll /framework:.NETFramework,Version=v4.6 /platform:x64 /Diag:diag.txt
+
+echo %vstest%
 echo "ERRORLEVEL=%ERRORLEVEL%"
 pause
